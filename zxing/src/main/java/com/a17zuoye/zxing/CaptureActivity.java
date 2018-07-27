10e49e30799806690e44aa132dca8d03f25ec136
jieyu.chen <jieyu.chen@17zuoye.com>
2018-07-27 21:06:36 +0800

    add zxing

---
+++ zxing/src/main/java/com/a17zuoye/zxing/CaptureActivity.java
@@ -0,0 +1,275 @@
+/*
+ * Copyright (C) 2008 ZXing authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.a17zuoye.zxing;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import com.a17zuoye.zxing.camera.CameraManager;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.DecodeHintType;
+import com.google.zxing.Result;
+import com.yiqizuoye.manager.EventCenterManager;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * This activity opens the camera and does the actual scanning on a background thread. It draws a
+ * viewfinder to help the user place the barcode correctly, shows feedback as the image processing
+ * is happening, and then overlays the results when a scan is successful.
+ *
+ * @author dswitkin@google.com (Daniel Switkin)
+ * @author Sean Owen
+ */
+public final class CaptureActivity extends Activity implements SurfaceHolder.Callback {
+
+    private static final String TAG = CaptureActivity.class.getSimpleName();
+
+    public static final int EVENT_SCAN_COMPLETE = 40014;
+    public static final String SCAN_RESULT = "SCAN_RESULT";
+
+    private CameraManager cameraManager;
+    private CaptureActivityHandler handler;
+    private Result savedResultToShow;
+    private ViewfinderView viewfinderView;
+    private TextView statusView;
+    private TextView loadingText;
+    private boolean hasSurface;
+    private Collection<BarcodeFormat> decodeFormats;
+    private Map<DecodeHintType, ?> decodeHints;
+    private String characterSet;
+    private AmbientLightManager ambientLightManager;
+
+    ViewfinderView getViewfinderView() {
+        return viewfinderView;
+    }
+
+    public Handler getHandler() {
+        return handler;
+    }
+
+    CameraManager getCameraManager() {
+        return cameraManager;
+    }
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.capture);
+
+        hasSurface = false;
+        ambientLightManager = new AmbientLightManager(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        cameraManager = new CameraManager(getApplication());
+
+        viewfinderView = (ViewfinderView) findViewById(R.id.viewfinder_view);
+        viewfinderView.setCameraManager(cameraManager);
+        loadingText = (TextView) findViewById(R.id.loading);
+        loadingText.setVisibility(View.VISIBLE);
+        statusView = (TextView) findViewById(R.id.status_view);
+        statusView.setVisibility(View.GONE);
+
+        handler = new CaptureActivityHandler(this, decodeFormats, decodeHints, characterSet, cameraManager);
+        cameraManager.openCamera(handler);
+
+        ambientLightManager.start(cameraManager);
+
+        decodeFormats = null;
+        characterSet = null;
+        isCameraComplete = false;
+        SurfaceView surfaceView = (SurfaceView) findViewById(R.id.preview_view);
+        SurfaceHolder surfaceHolder = surfaceView.getHolder();
+        if (hasSurface) {
+            // The activity was paused but not stopped, so the surface still exists. Therefore
+            // surfaceCreated() won't be called, so init the camera here.
+            initCamera(surfaceHolder);
+        } else {
+            // Install the callback and wait for surfaceCreated() to init the camera.
+            surfaceHolder.addCallback(this);
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        if (handler != null) {
+            handler.quitSynchronously();
+            handler = null;
+        }
+        ambientLightManager.stop();
+        cameraManager.closeDriver();
+        if (!hasSurface) {
+            SurfaceView surfaceView = (SurfaceView) findViewById(R.id.preview_view);
+            SurfaceHolder surfaceHolder = surfaceView.getHolder();
+            surfaceHolder.removeCallback(this);
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_FOCUS:
+            case KeyEvent.KEYCODE_CAMERA:
+                // Handle these events so they don't launch the Camera app
+                return true;
+            // Use volume up/down to turn on light
+            case KeyEvent.KEYCODE_VOLUME_DOWN:
+                cameraManager.setTorch(false);
+                return true;
+            case KeyEvent.KEYCODE_VOLUME_UP:
+                cameraManager.setTorch(true);
+                return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+
+    private void decodeOrStoreSavedBitmap(Bitmap bitmap, Result result) {
+        // Bitmap isn't used yet -- will be used soon
+        if (handler == null) {
+            savedResultToShow = result;
+        } else {
+            if (result != null) {
+                savedResultToShow = result;
+            }
+            if (savedResultToShow != null) {
+                Message message = Message.obtain(handler, R.id.decode_succeeded, savedResultToShow);
+                handler.sendMessage(message);
+            }
+            savedResultToShow = null;
+        }
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        if (holder == null) {
+            Log.e(TAG, "*** WARNING *** surfaceCreated() gave us a null surface!");
+        }
+        if (!hasSurface) {
+            hasSurface = true;
+            initCamera(holder);
+        }
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        hasSurface = false;
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        // do nothing
+    }
+
+    /**
+     * A valid barcode has been found, so give an indication of success and show the results.
+     *
+     * @param rawResult   The contents of the barcode.
+     * @param scaleFactor amount by which thumbnail was scaled
+     * @param barcode     A greyscale bitmap of the camera data which was decoded.
+     */
+    public void handleDecode(Result rawResult, Bitmap barcode, float scaleFactor) {
+        EventCenterManager.synSendEvent(new EventCenterManager.EventMessage(EVENT_SCAN_COMPLETE, rawResult.getText()));
+        Intent intent = new Intent();
+        intent.putExtra(SCAN_RESULT, rawResult.getText());
+        setResult(4044, intent);
+        finish();
+    }
+
+    private boolean isCameraComplete = false;
+
+    public void openCameraComplete() {
+        // 如果已有 SurfaceView 就可以继续创建 否则就等待SurfaceView 创建完以后自动执行
+        isCameraComplete = true;
+
+        if (hasSurface) {
+            SurfaceView surfaceView = (SurfaceView) findViewById(R.id.preview_view);
+            SurfaceHolder surfaceHolder = surfaceView.getHolder();
+            initCamera(surfaceHolder);
+        }
+    }
+
+
+    private void initCamera(SurfaceHolder surfaceHolder) {
+        if (surfaceHolder == null) {
+            throw new IllegalStateException("No SurfaceHolder provided");
+        }
+
+        if (!isCameraComplete) {
+            return;
+        }
+        if (cameraManager.isOpen()) {
+            Log.w(TAG, "initCamera() while already open -- late SurfaceView callback?");
+            return;
+        }
+        try {
+            cameraManager.openDriver(surfaceHolder);
+            handler.restartPreviewAndDecode();
+            loadingText.setVisibility(View.GONE);
+            statusView.setVisibility(View.VISIBLE);
+            decodeOrStoreSavedBitmap(null, null);
+        } catch (RuntimeException e) {
+            Log.w(TAG, "Unexpected error initializing camera", e);
+            displayFrameworkBugMessageAndExit();
+        } catch (IOException e) {
+            displayFrameworkBugMessageAndExit();
+
+        }
+    }
+
+    private void displayFrameworkBugMessageAndExit() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(getString(R.string.app_name));
+        builder.setMessage(getString(R.string.msg_camera_framework_bug));
+        builder.setPositiveButton("确定", new FinishListener(this));
+        builder.setOnCancelListener(new FinishListener(this));
+        builder.show();
+    }
+
+    public void drawViewfinder() {
+        viewfinderView.drawViewfinder();
+    }
+}
---
+++ zxing/src/main/java/com/a17zuoye/zxing/camera/open/OpenCameraInterface.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2012 ZXing authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.a17zuoye.zxing.camera.open;
+
+import android.hardware.Camera;
+import android.util.Log;
+
+import com.a17zuoye.zxing.CaptureActivityHandler;
+import com.a17zuoye.zxing.R;
+
+/**
+ * Abstraction over the {@link Camera} API that helps open them and return their metadata.
+ *
+ * by jieyu.chen
+ *
+ * 因为打开相机比较耗时，所以放在子线程中来
+ */
+@SuppressWarnings("deprecation") // camera APIs
+public final class OpenCameraInterface extends Thread {
+
+    private static final String TAG = OpenCameraInterface.class.getName();
+
+    private OpenCamera openCamera;
+
+    private CaptureActivityHandler handler;
+
+    public OpenCameraInterface(CaptureActivityHandler handler) {
+        this.handler = handler;
+    }
+
+    public OpenCamera getOpenCamera() {
+        return openCamera;
+    }
+
+    private OpenCamera open() {
+        int numCameras = Camera.getNumberOfCameras();
+        if (numCameras == 0) {
+            Log.w(TAG, "No cameras!");
+            return null;
+        }
+
+        int cameraId = 0;
+        while (cameraId < numCameras) {
+            Camera.CameraInfo cameraInfo = new Camera.CameraInfo();
+            Camera.getCameraInfo(cameraId, cameraInfo);
+            if (CameraFacing.values()[cameraInfo.facing] == CameraFacing.BACK) {
+                break;
+            }
+            cameraId++;
+        }
+        if (cameraId == numCameras) {
+            Log.i(TAG, "No camera facing " + CameraFacing.BACK + "; returning camera #0");
+            cameraId = 0;
+        }
+
+        Log.i(TAG, "Opening camera #" + cameraId);
+        Camera.CameraInfo cameraInfo = new Camera.CameraInfo();
+        Camera.getCameraInfo(cameraId, cameraInfo);
+        Camera camera = Camera.open(cameraId);
+        if (camera == null) {
+            return null;
+        }
+        return new OpenCamera(cameraId,
+                camera,
+                CameraFacing.values()[cameraInfo.facing],
+                cameraInfo.orientation);
+    }
+
+    @Override
+    public void run() {
+        openCamera = open();
+        handler.sendEmptyMessage(R.id.open_camera_complete);
+    }
+}
